608852f8844483e0cdec86b2bfec23aff19be88b
Aleksey <rusal@bk.ru>
2022-06-30 17:43:20 +0300

    serg

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Главная страница</h1>
+    <!-- <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    > -->
+
+  </q-page>
+
+</template>
+
+<script>
+import { defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+
+})
+</script>
